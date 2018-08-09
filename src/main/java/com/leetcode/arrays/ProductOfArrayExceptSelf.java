19f40fc65c747ac6410b01f3ccc57862232eccfa
yangyang <yangmeansyoung@gmail.com>
2018-08-09 22:14:12 +1000

    solution #238 https://leetcode.com/problems/product-of-array-except-self/description/

--- src/main/java/com/leetcode/arrays/ProductOfArrayExceptSelf.java
+++ src/main/java/com/leetcode/arrays/ProductOfArrayExceptSelf.java
@@ -1,11 +1,27 @@
 package com.leetcode.arrays;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductOfArrayExceptSelf {
     public int[] productExceptSelf(int[] nums) {
         int[] sum = new int[nums.length];
         int result = productResult(nums);
+        List<Integer> zeroIndecies = zeroIndecies(nums);
         for (int i = 0; i < nums.length; i++) {
-            sum[i] = result / nums[i];
+            if (zeroIndecies.size() == 1) {
+
+                if (zeroIndecies.contains(i)) {
+
+                    sum[i] = result;
+                }
+            }
+            else if (zeroIndecies.size() > 1) {
+                sum[i] = 0;
+            }
+            else {
+                sum[i] = result / nums[i];
+            }
         }
         return sum;
 
@@ -13,8 +29,22 @@ public class ProductOfArrayExceptSelf {
 
     private int productResult(int[] nums) {
         int result = 1;
+        boolean containsNoneZeo = false;
         for (int sumIndex = 0; sumIndex < nums.length; sumIndex++) {
-            result = result * nums[sumIndex];
+            if (nums[sumIndex] != 0) {
+                containsNoneZeo = true;
+                result = result * nums[sumIndex];
+            }
+        }
+        return containsNoneZeo ? result : 0;
+    }
+
+    private List<Integer> zeroIndecies(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        for (int sumIndex = 0; sumIndex < nums.length; sumIndex++) {
+            if (nums[sumIndex] == 0) {
+                result.add(sumIndex);
+            }
         }
         return result;
     }
--- src/test/java/com/leetcode/arrays/ProductOfArrayExceptSelfTest.java
+++ src/test/java/com/leetcode/arrays/ProductOfArrayExceptSelfTest.java
@@ -18,4 +18,14 @@ public class ProductOfArrayExceptSelfTest {
     public void productExceptSelf() {
         assertThat(classUnderTest.productExceptSelf(new int[]{1,2,3,4}), is(new int[]{24,12,8,6}));
     }
+
+    @Test
+    public void productExceptSelf1() {
+        assertThat(classUnderTest.productExceptSelf(new int[]{1, 0}), is(new int[]{0,1}));
+    }
+
+    @Test
+    public void productExceptSelf2() {
+        assertThat(classUnderTest.productExceptSelf(new int[]{0,4, 0}), is(new int[]{0, 0, 0}));
+    }
 }
