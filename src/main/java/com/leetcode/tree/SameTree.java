e44cd861149081f7e467e4a1397ed59ae1a5bdfc
yangyang <yangmeansyoung@gmail.com>
2018-08-28 07:39:43 +1000

    solution #100

--- src/main/java/com/leetcode/tree/SameTree.java
+++ src/main/java/com/leetcode/tree/SameTree.java
@@ -2,32 +2,67 @@ package com.leetcode.tree;
 
 public class SameTree {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-
-        if( !bothNodesAreTheSame(p, q)) {
+        if(p==null && q==null) {
+            return true;
+        }
+        if (isNodeValueTheSame(p, q) || !bothNodesAreTheSame(p, q)) {
             return false;
         }
 
-        if(isLeaf(p) && isLeaf(q)){
+        if (isLeaf(p) && isLeaf(q)) {
             return true;
         }
-    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    private boolean isNodeValueTheSame(TreeNode p, TreeNode q) {
+        if(p==null ^ q==null) {
+            return false;
+        }
+        if(p==null && q==null) {
+            return true;
+        }
+        return p.val!= q.val;
     }
 
     private boolean bothNodesAreTheSame(TreeNode p, TreeNode q) {
-
-        return isLeftLeafTheSame(p, q)
-                && isRightLeafTheSame(p,q );
+        if(p ==null && q ==null) {
+            return true;
+        }
+        if( p==null && q!=null) {
+            return false;
+        }
+        if( p !=null && q==null){
+            return false;
+        }
+        return  isLeftLeafTheSame(p, q)
+                && isRightLeafTheSame(p, q);
 
     }
 
     private boolean isLeftLeafTheSame(TreeNode p, TreeNode q) {
-        return p.left == null && q.left==null || p.left.val==q.left.val;
+        if(p.left==null ^ q.left==null) {
+            return false;
+        }
+
+        if(p.left == null && q.left == null ) {
+            return true;
+        }
+        return p.left.val == q.left.val;
     }
+
     private boolean isRightLeafTheSame(TreeNode p, TreeNode q) {
-        return p.right == null && q.right==null || p.right.val==q.right.val;
+        if(p.right==null ^ q.right==null) {
+            return false;
+        }
+
+        if(p.right == null && q.right == null ) {
+            return true;
+        }
+        return p.right.val == q.right.val;
     }
 
     private boolean isLeaf(TreeNode p) {
-        return p.left == null && p.right ==null;
+        return p.left == null && p.right == null;
     }
 }
--- src/test/java/com/leetcode/tree/SameTreeTest.java
+++ src/test/java/com/leetcode/tree/SameTreeTest.java
@@ -34,4 +34,31 @@ public class SameTreeTest {
         boolean sameTree = new SameTree().isSameTree(root1, root2);
         assertTrue(sameTree);
     }
+
+    @Test
+    public void notSameTree2() {
+        TreeNode root1 = new TreeNode(1);
+        root1.left = new TreeNode(2);
+
+        TreeNode root2 = new TreeNode(1);
+        root2.left = null;
+        root2.right = new TreeNode(3);
+
+        boolean sameTree = new SameTree().isSameTree(root1, root2);
+        assertFalse(sameTree);
+    }
+
+    @Test
+    public void notSameTree3() {
+        TreeNode root1 = new TreeNode(1);
+        root1.left = null;
+        root1.right = null;
+
+        TreeNode root2 = new TreeNode(2);
+        root2.left = null;
+        root2.right =null;
+
+        boolean sameTree = new SameTree().isSameTree(root1, root2);
+        assertFalse(sameTree);
+    }
 }
